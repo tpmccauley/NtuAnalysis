fe86996989cfedf58efdf964553703fd95a3599f
Paolo <paolo.ronchese@cern.ch>
2015-10-02 14:21:28 +0200

    EvNumFilter added to update list for CMSSW_7

--- uty/updateTo7.sh
+++ uty/updateTo7.sh
@@ -6,4 +6,7 @@ export TOP=`dirname ${DIR}`
 ${DIR}/changeReturnType.sh ${TOP}/Write/interface/NtuFilter.h
 ${DIR}/changeReturnType.sh ${TOP}/Write/interface/NtuFilter.hpp
 
+${DIR}/changeReturnType.sh ${TOP}/Write/plugins/EvNumFilter.h
+${DIR}/changeReturnType.sh ${TOP}/Write/plugins/EvNumFilter.cc
+
 eval `find ${TOP} | grep BuildFile | grep 'xml$' | awk -v DIR=${DIR} '{print DIR"/cleanupBuildFile.sh "$0" ;"}'`
