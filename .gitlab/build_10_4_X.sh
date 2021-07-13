00fe181fe1d6cc68b2cc4ef20a604b4cee21c0ce
Tom McCauley <tpmccauley@gmail.com>
2021-07-13 09:34:00 +0100

    try build again

--- .gitlab/build_10_4_X.sh
+++ .gitlab/build_10_4_X.sh
@@ -4,9 +4,16 @@ set -ex
 shopt -s expand_aliases
 export BUILD_DIR=${PWD}
 set +u && source ${CMS_PATH}/cmsset_default.sh; set -u
-ls -ltr
-source ${BUILD_DIR}/uty/setAtA2E.sh
+source ${BUILD_DIR}/uty/setAtA40.sh
 cmsrel ${CMSSW_RELEASE}
 cd ${CMSSW_RELEASE}/src
 git clone https://github.com/tpmccauley/NtuTool.git NtuTool
+mkdir -p NtuAnalysis
+mv ${BUILD_DIR}/Common
+mv ${BUILD_DIR}/Read
+mv ${BUILD_DIR}/Template
+mv ${BUILD_DIR}/Write
+mv ${BUILD_DIR}/uty
 ls -ltr
+cmsenv
+scram b -v
